a0e59446a10a9f1a74cf1d844c7b0f3abc777d41
Saurav <sauravsingh161@gmail.com>
2026-03-08 14:49:56 +0530

    Initial JMeter performance test

---
+++ run_test.bat
@@ -0,0 +1,2 @@
+jmeter -n -t C:\Jmeter_API_Hoverflytest\K8_DOCKERTESTS\AWS_Scenario.jmx -l C:\Jmeter_API_Hoverflytest\K8_DOCKERTESTS\result3.jtl
+jmeter -g C:\Jmeter_API_Hoverflytest\K8_DOCKERTESTS\result3.jtl -o C:\Jmeter_API_Hoverflytest\K8_DOCKERTESTS\report

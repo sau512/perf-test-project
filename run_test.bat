ce55ab5478d78918defc71b6b5895e38b004933b
Saurav <sauravsingh161@gmail.com>
2026-03-08 20:32:30 +0530

    updated jmeter report generation

--- run_test.bat
+++ run_test.bat
@@ -1,2 +1,11 @@
-jmeter -n -t C:\Jmeter_API_Hoverflytest\K8_DOCKERTESTS\AWS_Scenario.jmx -l C:\Jmeter_API_Hoverflytest\K8_DOCKERTESTS\result3.jtl
-jmeter -g C:\Jmeter_API_Hoverflytest\K8_DOCKERTESTS\result3.jtl -o C:\Jmeter_API_Hoverflytest\K8_DOCKERTESTS\report
+echo Starting JMeter Test...
+
+jmeter -n -t AWS_Scenario.jmx -l result.jtl
+
+echo Cleaning old report...
+
+rmdir /s /q report
+
+echo Generating HTML report...
+
+jmeter -g result.jtl -o report
